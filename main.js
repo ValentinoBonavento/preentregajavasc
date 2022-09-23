6ffde37cce285608bff34f0ab9f87a1794ac7e3b
valentino bonavento <valentino.bonavento@gmail.com>
2022-09-22 22:36:07 -0300

    entrega

--- main.js
+++ main.js
@@ -6,33 +6,7 @@ class Persona {
         this.actividad = actividad;
     }
 
-    deporteIntenso() {
-        let calorias = (this.peso * 3500)
-        let dias11 = (calorias / 900).toFixed(0)
-        console.log(`Tendrias que hacer ${dias11} dias de deporte intenso para perder ${peso} kilos, bajo una dieta de deficit calorico`)
-        return dias11
-    }
     
-    deporteLeve() {
-        let calorias = (this.peso * 3500)
-        let dias12 = (calorias / 500).toFixed(0)
-        console.log(`Tendrias que hacer ${dias12} dias de deporte leve para perder ${peso} kilos, bajo una dieta de deficit calorico`)
-        return dias12
-    }
-    
-    gimnasioIntenso() {
-        let calorias = (this.peso * 3500)
-        let dias21 = (calorias / 700).toFixed(0)
-        console.log(`Tendrias que hacer ${dias21} dias de gimnasio intenso para perder ${peso} kilos, bajo una dieta de deficit calorico`)
-        return dias21
-    }
-    
-    gimnasioLeve() {
-        let calorias = (this.peso * 3500)
-        let dias22 = (calorias / 300).toFixed(0)
-        console.log(`Tendrias que hacer ${dias22} dias de gimnasio leve para perder ${peso} kilos, bajo una dieta de deficit calorico`)
-        return dias22
-    }
     
 }
 const personas = []
@@ -54,26 +28,61 @@ function agregar_persona() {
     personas.push(persona)
     localStorage.setItem("personas", JSON.stringify(personas))
     formulario.reset();
+    if (intensidad == 1 && actividad == 1) {
+        deporteIntenso()
+    } else if (intensidad == 1 && actividad == 2) {
+        deporteLeve()
+    } else if (intensidad == 2 && actividad == 1) {
+        gimnasioIntenso()
+    } else if (intensidad == 2 && actividad == 2) {
+        gimnasioLeve()
+    }
+
+    function deporteIntenso() {
+        let calorias = (peso * 3500)
+        let dias = (calorias / 900).toFixed(1)
+        console.log(`Tendrias que hacer ${dias} dias de deporte intenso para perder ${peso} kilos, bajo una dieta de deficit calorico`)
+        let aux = ""
+        aux += `<p class="resultado"> Tendrias que hacer ${dias} dias de deporte leve para perder ${peso} kilos, bajo una dieta de deficit calorico </p>`
+        resultado.innerHTML = aux;
+    }
+    
+    function deporteLeve() {
+        let calorias = (peso * 3500)
+        let dias = (calorias / 500).toFixed(0)
+        console.log(`Tendrias que hacer ${dias} dias de deporte leve para perder ${peso} kilos, bajo una dieta de deficit calorico`)
+        let aux = ""
+        aux += `<p class="resultado"> Tendrias que hacer ${dias} dias de deporte leve para perder ${peso} kilos, bajo una dieta de deficit calorico </p>`
+        resultado.innerHTML = aux;
+         
+    }
+    
+    function gimnasioIntenso() {
+        let calorias = (peso * 3500)
+        let dias = (calorias / 700).toFixed(0)
+        console.log(`Tendrias que hacer ${dias} dias de gimnasio intenso para perder ${peso} kilos, bajo una dieta de deficit calorico`)
+        let aux = ""
+        aux += `<p class="resultado"> Tendrias que hacer ${dias} dias de deporte leve para perder ${peso} kilos, bajo una dieta de deficit calorico </p>`
+        resultado.innerHTML = aux; 
+    }
+    
+    function gimnasioLeve() {
+        let calorias = (peso * 3500)
+        let dias = (calorias / 300).toFixed(0)
+        console.log(`Tendrias que hacer ${dias} dias de gimnasio leve para perder ${peso} kilos, bajo una dieta de deficit calorico`)
+        let aux = ""
+        aux += `<p class="resultado"> Tendrias que hacer ${dias} dias de deporte leve para perder ${peso} kilos, bajo una dieta de deficit calorico </p>`
+        resultado.innerHTML = aux; 
+    }
 }
 
 const resultado = document.getElementById("infoUsuarios");
 
-const mostrar_info = (persona) => {
-    let aux = "";
-    aux += `<p class="resultado"> ${persona.nombre} tu IMC es el siguiente: </p>`
-    if (intensidad == 1 && actividad == 1) {
-        `<p class="resultado"> IMC: ${persona.deporteIntenso()} </p>`
-    } else if (intensidad == 1 && actividad == 2) {
-        `<p class="resultado"> IMC: ${persona.gimnasioIntenso()} </p>`
-    } else if (intensidad == 2 && actividad == 1) {
-        `<p class="resultado"> IMC: ${persona.deporteLeve()} </p>`
-    } else if (intensidad == 2 && actividad == 2) {
-        `<p class="resultado"> IMC: ${persona.gimnasioLeve()} </p>`
-    }
-    resultado.innerHTML = aux;
-}
 
-
+    
+    
+  
+    
 console.log(personas)
 personas.forEach(function (contexto) {
     console.log(contexto)
